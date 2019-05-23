8dcc878d56dc9c393632edb67402aa7548fc8cdd
Alexander Knechtl <alex.knechtl@gmx.at>
2019-05-23 17:06:33 +0200

    Quick Push.

--- src/components/common/StatisticsCategory.js
+++ src/components/common/StatisticsCategory.js
@@ -8,50 +8,56 @@ import '../styles.css';
 const icSucceded = require('../../img/ic_check_green.png')
 const icFailed = require('../../img/ic_wrong_red.png')
 
-const StatisticsCategory = ({ titleText, questionsRight, success, imageUri, questionsFalse, learningState }) => {
+const StatisticsCategory = ({ titleText, questionsRight, success, imageUri, questionsFalse, learningState, onPress }) => {
     return (
-        <div style={{
-            backgroundImage: `url(${imageUri})`,
-            backgroundSize: '110%',
-            textDecorationLine: 'none',
-            textDecoration: 'none',
-            textDecorationColor: '#fff0',
-        }}>
-            <p style={titleTextStyle}>
-                {titleText}
-            </p>
-            <div style={{ display: 'flex', alignItems: 'center', flexDirection: "row", marginTop: 8, justifyContent: "space-between" }}>
-                <p style={questionInfo}>
-                    {questionsRight} Fragen {"\n"}richtig
-            </p>
-                <div style={{
-                    width: 58,
-                    height: 58,
-                    borderRadius: 30,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: '#fff0',
-                    borderColor: success ? '#2EEF6A' : '#F44B4B',
-                    borderWidth: 2
-                }}>
-                    <img style={icon} src={success ? icSucceded : icFailed} />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div style={{
+                backgroundImage: `url(${imageUri})`,
+                backgroundSize: '110%',
+                textDecorationLine: 'none',
+                textDecoration: 'none',
+                textDecorationColor: '#fff0'
+            }}>
+                <p numberOfLines={1} style={title}> {titleText} </p>
+                <div style={{ display: 'flex', alignItems: 'center', flexDirection: "row", marginTop: 8, justifyContent: "space-between" }}>
+                    <div style={{
+                        width: 58,
+                        height: 58,
+                        borderRadius: 30,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        backgroundColor: '#fff0',
+                        borderColor: success ? '#2EEF6A' : '#F44B4B',
+                        borderWidth: 2
+                    }}>
+                        <img style={icon} src={success ? icSucceded : icFailed} />
+                    </div>
                 </div>
-                <p style={{ color: '#fff', fontSize: 20, fontFamily: 'Roboto Slab', textAlign: "right" }}>
-                    {questionsFalse} Fragen {"\n"}falsch
-            </p>
             </div>
-            <Progress style={{ minHeight: 24, width: "100%" }} width={null} progress={learningState} height={24} color={'#2EEF6A'} unfilledColor='#F44B4B' borderWidth={0} borderRadius={0}>
+            <Progress style={{ minHeight: 24, width: "100%" }} width={null} progress={30} height={24} color={'#2EEF6A'} unfilledColor='#F44B4B' borderWidth={0} borderRadius={0}>
                 <p style={{ marginLeft: 12, marginTop: -24, marginBottom: -2, color: "#fff", width: "100%", fontFamily: 'Roboto Slab', fontWeight: "bold", fontSize: 18 }}>{(learningState * 100).toFixed(0)} %</p>
             </Progress>
         </div>
     )
 }
 
-const containerStyle = {
-    height: 128,
-    paddingHorizontal: 12,
-    paddingVertical: 2
+const title = {
+    color: "#fff",
+    fontWeight: "bold",
+    backgroundColor: "#fff3",
+    marginTop: 0,
+    marginBottom: '0.85em',
+    fontSize: "0.7em",
+    maxLines: 1,
+    maxHeight: '1.3em',
+    height: '1.3em',
+    overflow: "hidden",
+    textAlign: "left",
+    paddingLeft: "0.6em",
+    paddingTop: '0.3em',
+    paddingBottom: '0.3em'
 }
+
 
 const textErfolg = {
     fontSize: 14,
