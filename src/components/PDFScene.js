82e6f8f761efda2c144609b9dd011afcdda418f6
Alexander Knechtl <alex.knechtl@gmx.at>
2019-05-24 18:28:10 +0200

    Some Improvements in PDF Scene.
    PDF Scene - Cant change title length

--- src/components/PDFScene.js
+++ src/components/PDFScene.js
@@ -17,7 +17,7 @@ export default class PDFScene extends Component {
     let pageNum = params.get('page') || 1;
     let url = atob(params.get('url'));
 
-    this.state = { url, pageNumber: parseInt(pageNum), numPages: null , pageHeight: 1000};
+    this.state = { url, pageNumber: parseInt(pageNum), numPages: null, pageHeight: 1000 };
   }
 
   onDocumentLoadSuccess = ({ numPages }) => {
@@ -26,48 +26,44 @@ export default class PDFScene extends Component {
   };
 
   goToPrevPage = () =>
-    this.setState(state => ({ pageNumber: state.pageNumber>1?state.pageNumber - 1:1 }));
+    this.setState(state => ({ pageNumber: state.pageNumber > 1 ? state.pageNumber - 1 : 1 }));
 
   goToNextPage = () =>
     this.setState(state => ({ pageNumber: state.pageNumber < state.numPages ? state.pageNumber + 1 : state.numPages }));
 
-    goToFirstPage = () =>
+  goToFirstPage = () =>
     this.setState(state => ({ pageNumber: 1 }));
 
   goToLastPage = () =>
     this.setState(state => ({ pageNumber: state.numPages }));
 
-  toogleModal = ()=> {
+  toogleModal = () => {
     this.refs.popupLoading.openModal();
   }
 
   closeModal() {
     this.refs.popupLoading.closeModal();
   }
-  /*
-    <p>
-    Page {pageNumber} of {numPages}
-  </p>
-  <nav>
-    <button onClick={this.goToPrevPage}>Prev</button>
-    <button onClick={this.goToNextPage}>Next</button>
-  </nav> */
 
   render() {
     const { pageNumber, numPages, url } = this.state;
-    
+
     let params = new URLSearchParams(this.props.location.search);
+    let skriptName = decodeURI(params.get('pdfname'));
 
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1, textAlign: 'center', backgroundColor: '#003A65', height: `${this.state.pageHeight+500}` }}>
-        <div style={{ width: '100%', height: '3.5em', backgroundColor: '#032C4A', position: 'fixed', textAlign: 'left' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1, textAlign: 'center', backgroundColor: '#003A65', minHeight: '100vh', height: `${this.state.pageHeight + 500}` }}>
+        <div style={{ width: '100%', height: '3.5em', backgroundColor: '#032C4A', position: 'fixed', textAlign: 'left'}}>
           <p style={{
             color: '#fff',
             fontSize: 18,
+            maxWidth: '20%',
             fontWeight: 'bold',
-            marginLeft: 24
+            marginLeft: 24,
+            overflow: 'hidden',
+            textAlign: 'left'
           }}>
-            { decodeURI(params.get('pdfname'))||"Skriptum über irgendwas"}
+            {skriptName}
           </p>
           <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'absolute', left: '50%', top: '0em', transform: 'translateX(-50%)' }}>
             <img
@@ -95,14 +91,14 @@ export default class PDFScene extends Component {
               alt="Error" />
           </div>
 
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'absolute', right: '10%', top: "0.5em"}}>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'absolute', right: '10%', top: "0.5em" }}>
             <img
-              onClick={()=>this.setState(state=>({pageHeight: state.pageHeight + 100}))}
+              onClick={() => this.setState(state => ({ pageHeight: state.pageHeight + 100 }))}
               src={zoomIn}
-              style={{ width: 40, height: 40, marginLeft: 0, marginTop: 0, marginBottom: 0}}
+              style={{ width: 40, height: 40, marginLeft: 0, marginTop: 0, marginBottom: 0 }}
               alt="Error" />
             <img
-              onClick={()=>this.setState(state=>({pageHeight: state.pageHeight - 100}))}
+              onClick={() => this.setState(state => ({ pageHeight: state.pageHeight - 100 }))}
               src={zoomOut}
               style={{ width: 40, height: 40, marginTop: 0, marginBottom: 0, marginRight: 0 }}
               alt="Error" />
@@ -112,7 +108,7 @@ export default class PDFScene extends Component {
         <div style={{ display: 'flex', justifyContent: 'center', width: '100%', marginTop: "5em", textAlign: 'center', paddingBottom: "2em" }}>
           <Document
             file={url}
-            loading={(props) => {this.toogleModal.bind(this); return <p></p>}}
+            loading={(props) => { this.toogleModal.bind(this); return <p></p> }}
             onLoadError={(error) => console.log(error)}
             onLoadSuccess={this.onDocumentLoadSuccess}
             onSourceSuccess={() => console.log('Document received')}
@@ -127,7 +123,7 @@ export default class PDFScene extends Component {
             fontWeight: 'bold',
             marginLeft: 24
           }}>
-            { decodeURI(params.get('pdfname'))||"Skriptum über irgendwas"}
+            {decodeURI(params.get('pdfname')) || "Skriptum über irgendwas"}
           </p>
           <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'absolute', left: '50%', top: '0em', transform: 'translateX(-50%)' }}>
             <img
@@ -155,19 +151,18 @@ export default class PDFScene extends Component {
               alt="Error" />
           </div>
 
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'absolute', right: '10%', top: "0.5em"}}>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'absolute', right: '10%', top: "0.5em" }}>
             <img
-              onClick={()=>this.setState(state=>({pageHeight: state.pageHeight + 100}))}
+              onClick={() => this.state.pageHeight < 2800 ? this.setState(state => ({ pageHeight: state.pageHeight + 200 })) : ""}
               src={zoomIn}
-              style={{ width: 40, height: 40, marginLeft: 0, marginTop: 0, marginBottom: 0}}
+              style={{ width: 40, height: 40, marginLeft: 0, marginTop: 0, marginBottom: 0 }}
               alt="Error" />
             <img
-              onClick={()=>this.setState(state=>({pageHeight: state.pageHeight - 100}))}
+              onClick={() => this.state.pageHeight > 600 ? this.setState(state => ({ pageHeight: state.pageHeight - 200 })) : ""}
               src={zoomOut}
               style={{ width: 40, height: 40, marginTop: 0, marginBottom: 0, marginRight: 0 }}
               alt="Error" />
           </div>
-
         </div>
         <LoadingPopup ref={'popupLoading'} />
       </div>
